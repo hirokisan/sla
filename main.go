3696bfd787f491c6f3958bb22826d81bbf38fb4b
hirokisan <expsharetoworld@gmail.com>
2019-01-14 09:33:34 +0000

    feat: init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/nlopes/slack"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app  = kingpin.New("chat", "A command-line chat application.")
+	post = kingpin.Command("post", "post message to slack")
+	url  = post.Flag("url", "incomming webhook url").Short('u').String()
+)
+
+func main() {
+	switch kingpin.Parse() {
+	case "post":
+		if *url != "" {
+			message := Message("this is message")
+			slack.PostWebhook(*url, message)
+		}
+	}
+}
+
+func Message(m string) *slack.WebhookMessage {
+	return &slack.WebhookMessage{
+		Text:        m,
+		Attachments: []slack.Attachment{},
+	}
+}
